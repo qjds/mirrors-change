b2c4716b14ac742b8e72a72a63a761905afa5287
qjds <81950179+qjds@users.noreply.github.com>
2024-09-06 09:08:41 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,8 +2,9 @@
 echo -e "\e[32m
 1.ustc		:切换中科大源并更新索引
 2.aliyun	:切换阿里云源并更新索引 (谨慎选择，目前限速)
-3.docker-ce	:使用中科大源安装docker
-4.minio		:docker导入minio镜像到imges
+3.input     :自定义源替换
+4.docker-ce	:使用中科大源安装docker
+5.minio		:docker导入minio镜像到imges
 
 你目前正在使用的源是$(awk '$1 ~ /^deb/ {print $2}' /etc/apt/sources.list | head -n 1)
 \e[0m"
@@ -19,7 +20,7 @@ change-mirror(){
     fi
 }
 
-read -p "输入选项:" op
+read -p "输入选项：" op
 
 case $op in
 
@@ -32,16 +33,20 @@ change-mirror "https://mirrors.aliyun.com/ubuntu"
 ;;
 
 3)
+echo -e "\e[33m输入你要替换的源地址，要确保该地址下有 dists 文件夹\n\e[0m"
+read -p "输入需要替换的源：" mirror
+change-mirror "$mirror"
+;;
+
+4)
 sed -i '/docker-ce/d' /etc/apt/sources.list
-#curl -Ls https://download.qijds.top/docker-ce.gpg | gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/docker-ce.gpg
 curl -Ls https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | gpg --dearmor --yes -o  /etc/apt/trusted.gpg.d/docker-ce.gpg
-lsb=$(lsb_release -cs)
-echo "deb https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu $lsb stable" >> /etc/apt/sources.list
+echo "deb https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable" >> /etc/apt/sources.list
 apt update
 apt install docker-ce -y
 ;;
 
-4)
+5)
 wget https://download.qijds.top/minio.tar 
 docker load < minio.tar
 rm minio.tar

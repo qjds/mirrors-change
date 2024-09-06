c8e7943031b284e6d76c1c8918507478ead1b744
qjds <81950179+qjds@users.noreply.github.com>
2024-09-06 16:04:19 +0800

    自定义源，自定义容器镜像，netplan静态ip

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,14 +5,16 @@ echo -e "\e[32m
 0.exit		:退出本脚本
 1.ustc		:切换中科大源并更新索引
 2.aliyun	:切换阿里云源并更新索引 (谨慎选择，目前限速)
-3.input		:自定义源替换
+3.mirrors	:自定义源替换
 4.docker-ce	:使用中科大源安装docker
 5.minio		:docker导入minio镜像到imges
+6.images	:导入自定义docker镜像
+7.network	:修改netplan配置为静态地址
 
 你目前正在使用的源是$(awk '$1 ~ /^deb/ {print $2}' /etc/apt/sources.list | head -n 1)
 \e[0m"
 
-change-mirror(){
+change-mirrors(){
 	echo -e "\e[31m\n确定要更改为？$1\n确定输入 y 取消输入 n 默认y\n\e[0m"
 	read -p "输入选项: " yorn
 	if [[ "$yorn" = "n" ]]; then
@@ -23,22 +25,29 @@ change-mirror(){
 	fi
 }
 
+load-images(){
+	wget $1
+	file=$(echo $1 | sed 's|.*/||')
+	docker load < $file
+	rm $file
+}
+
 read -p "输入选项：" op
 
 case $op in
 
 1)
-change-mirror "https://mirrors.ustc.edu.cn/ubuntu"
+change-mirrors "https://mirrors.ustc.edu.cn/ubuntu"
 ;;
 
 2)
-change-mirror "https://mirrors.aliyun.com/ubuntu"
+change-mirrors "https://mirrors.aliyun.com/ubuntu"
 ;;
 
 3)
 echo -e "\e[33m输入你要替换的源地址，要确保该地址下有 dists 文件夹\n\e[0m"
-read -p "输入需要替换的源：" mirror
-change-mirror "$mirror"
+read -p "输入需要替换的源URL：" mirrors
+change-mirror "$mirrors"
 ;;
 
 4)
@@ -50,9 +59,43 @@ apt install docker-ce -y
 ;;
 
 5)
-wget https://download.qijds.top/minio.tar 
-docker load < minio.tar
-rm minio.tar
+load-images "https://download.qijds.top/minio.tar"
+;;
+
+6)
+echo -e "\e[33m输入你要导入的docker镜像，要确保该地址可以直接下载\n\e[0m"
+read -p "输入导入的镜像URL：" images
+load-images "$images"
+;;
+
+7)
+wk=$(ip r | grep via | cut -d ' ' -f 5)
+ip=$(ifconfig $wk | grep netmask | awk '$1=$1' | cut -d ' ' -f 2)
+ym=$(ip r | grep $ip | cut -d '/' -f 2 | cut -d ' ' -f 1)
+wg=$(ip r | grep via | cut -d ' ' -f 3)
+cat << EOF > /etc/netplan/*cloud-init.yaml
+network:
+    ethernets:
+        $wk:
+            addresses:
+            - $ip/$ym
+            nameservers:
+                addresses:
+                - 223.6.6.6
+                search:
+                - dns.alidns.com
+            routes:
+            -   to: default
+                via: $wg
+        ens192:
+            addresses:
+            - 192.168.10.250/24
+            nameservers:
+                addresses: []
+                search: []
+    version: 2
+EOF
+netplan apply
 ;;
 
 10)

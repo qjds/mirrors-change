40f47dc546154fa7e1f77c5d0702fa0c1419ed16
qjds <2731849985@qq.com>
2024-10-16 11:50:30 +0800

    net1

--- ubuntu.sh
+++ ubuntu.sh
@@ -79,7 +79,8 @@ load-images "$images"
 wk=$(ip r | grep via | sed -n "2p" | cut -d ' ' -f 5)
 ip=$(ip a | grep $wk | sed -n "2p" | awk '{print $2}')
 wg=$(ip r | grep via | head -n 1 | cut -d ' ' -f 3)
-cat << EOF > $(find /etc/netplan/ -name *.yaml | head -n 1)
+file=$(find /etc/netplan/ -name *.yaml | head -n 1)
+cat << EOF > $(file)
 network:
     ethernets:
         $wk:
